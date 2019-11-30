76669bebff9457d43a731a37308b52663df5135d
ahsanhabib91 <habibibnakther@gmail.com>
2019-11-30 22:41:47 +0600

    Done Lec-09

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -24,12 +25,20 @@ io.on('connection', (socket) => {
 
 	socket.broadcast.emit('message', 'A new user has joined!'); // sending to all clients except sender
 
-	socket.on('sendMessage', message => {
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed!')
+		}
+
 		io.emit('message', message); // emit the event to every client
+		callback(); // sending with acknowledgement
 	});
 
-	socket.on('sendLocation', coords => {
+	socket.on('sendLocation', (coords, callback) => {
 		io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+		callback();
 	});
 
 	socket.on('disconnect', () => {
